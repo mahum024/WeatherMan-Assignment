28cc652f5983487ee96ed299c02ccf1b657c8f89
Mahum0987654321 <108531468+Mahum0987654321@users.noreply.github.com>
2022-07-05 11:19:38 +0500

    Changes made as required

--- file_read.rb
+++ file_read.rb
@@ -1,33 +1,21 @@
 # frozen_string_literal: true
-
 module FileRead
   require_relative 'horizontal_charts'
   require_relative 'calculate_tempratures'
   include DisplayHorizontalCharts
   include CalculateTempratureModule
   require 'date'
+
   def read_by_years
-    index_of_line = 0
     highest = []
     lowest = []
-    line_data = []
     humid = []
     date = []
+    read_year_data = [highest, lowest, humid, date]
 
     (1..12).step(1) do |month|
       file = "#{@file_path}/#{@file_path}_#{@time}_#{Date::ABBR_MONTHNAMES[month]}.txt"
-      if File.exist?(file)
-        file = File.open(file)
-        file.readline
-        file.readlines.each do |file_data|
-          line_data[index_of_line] = file_data.split(',')
-          highest.push(line_data[index_of_line][1])
-          lowest.push(line_data[index_of_line][3])
-          humid.push(line_data[index_of_line][9])
-          date.push(line_data[index_of_line][0])
-          index_of_line += 1
-        end
-      end
+      read_file_data(file, read_year_data) if File.exist?(file)
     end
 
     calculate_max_temprature(highest, date)
@@ -35,59 +23,47 @@ module FileRead
     calculate_max_humid(humid, date)
   end
 
-  def read_by_year_and_month_a
+  def read_by_month
     year_month = @time.split('/')
-    file_name = "#{@file_path}/#{@file_path}_#{year_month[0].to_i}_#{Date::ABBR_MONTHNAMES[(year_month[1].to_i)]}.txt"
-
-    index_of_line = 0
+    file = "#{@file_path}/#{@file_path}_#{year_month[0].to_i}_#{Date::ABBR_MONTHNAMES[(year_month[1].to_i)]}.txt"
 
     highest = []
     lowest = []
-    line_data = []
     humid = []
     date = []
+    read_data = [highest, lowest, humid, date]
 
-    if File.exist?(file_name)
-      file = File.open(file_name)
-      file.readline
-      file.readlines.each do |file_data|
-        line_data[index_of_line] = file_data.split(',')
-        highest << line_data[index_of_line][1]
-        lowest << line_data[index_of_line][3]
-        humid << line_data[index_of_line][8]
-        date << line_data[index_of_line][0]
-        index_of_line += 1
+    if File.exist?(file)
+      read_file_data(file, read_data)
+
+      case @read_option
+      when '-a'
+        calculate_highest_avg(highest)
+        calculate_lowest_avg(lowest)
+        calculate_avg_humid(humid)
+      when '-c'
+        arg = [date, highest, lowest, Date::ABBR_MONTHNAMES[(year_month[1].to_i)], year_month[0].to_i]
+        print_highest_and_lowest(arg)
+        highest_lowest_combine(arg)
       end
-
-      calculate_highest_avg(highest)
-      calculate_lowest_avg(lowest)
-      calculate_avg_humid(humid)
 
     end
   end
 
-  def read_by_year_and_month_c
-    year_month = @time.split('/')
-    file_name = "#{@file_path}/#{@file_path}_#{year_month[0].to_i}_#{Date::ABBR_MONTHNAMES[(year_month[1].to_i)]}.txt"
+  def read_file_data(file, read_data)
 
+    line_data = []
     index_of_line = 0
 
-    high = []
-    lowest = []
-    line_data = []
-    date = []
-
-    file = File.open(file_name)
+    file = File.open(file)
     file.readline
+
     file.readlines.each do |file_data|
       line_data[index_of_line] = file_data.split(',')
-      high << line_data[index_of_line][1]
-      lowest << line_data[index_of_line][3]
-      date << line_data[index_of_line][0]
-      index_of_line += 1
+      read_data[0].push(line_data[index_of_line][1])
+      read_data[1].push(line_data[index_of_line][3])
+      read_data[2].push(line_data[index_of_line][9])
+      read_data[3].push(line_data[index_of_line][0])
     end
-    arg = [date, high, lowest, Date::ABBR_MONTHNAMES[(year_month[1].to_i)], year_month[0].to_i]
-    print_highest_and_lowest(arg)
-    highest_lowest_combine(arg)
   end
 end
