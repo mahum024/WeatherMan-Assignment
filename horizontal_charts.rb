7850c012cabb0c73b7930cfb5ff0f632e0ab19bf
Mahum0987654321 <108531468+Mahum0987654321@users.noreply.github.com>
2022-07-05 11:20:19 +0500

    changes made as required

--- horizontal_charts.rb
+++ horizontal_charts.rb
@@ -3,42 +3,23 @@
 require 'colorize'
 
 module DisplayHorizontalCharts
-  def print_highest_and_lowest(arg)
-    date = arg[0]
-    highest = arg[1]
-    lowest = arg[2]
-    month = arg[3]
-    year = arg[4]
 
-    day = []
-    date.each do |d|
-      day << d.split('-')
-    end
-    puts("#{month}  #{year}")
-    date.length.times do |values|
-      print "#{day[values][2]}  #{('+' * highest[values].to_i).to_s.red}#{highest[values]}C\n"
-      print "#{day[values][2]}  #{('+' * lowest[values].to_i).to_s.blue}#{lowest[values]}C\n"
+  def print_highest_and_lowest(arg)
+    puts("#{arg[3]}  #{arg[4]}")
+
+    30.times do |values|
+      print "#{values}  #{('+' * arg[1][values].to_i).to_s.red}#{arg[1][values]}C\n"
+      print "#{values}  #{('+' * arg[2][values].to_i).to_s.blue}#{arg[2][values]}C\n"
     end
   end
 
   def highest_lowest_combine(arg)
-    date = arg[0]
-    highest = arg[1]
-    lowest = arg[2]
-    month = arg[3]
-    year = arg[4]
-
-    day = []
-    date.each do |d|
-      day << d.split('-')
-    end
-
-    # Bonus task
     puts 'BONUS TASK '
-    puts("#{month}  #{year}")
+    puts("#{arg[3]}  #{arg[4]}")
+
     2.times do |values|
-      print "#{day[values][2]}  #{('+' * lowest[values].to_i).to_s.blue}#{('+' * highest[values].to_i).to_s.red}"
-      print "#{lowest[values]}C-#{highest[values]}C\n"
+      print "#{values}  #{('+' * arg[2][values].to_i).to_s.blue}#{('+' * arg[1][values].to_i).to_s.red}"
+      print "#{arg[2][values]}C-#{arg[1][values]}C\n"
     end
   end
 end
